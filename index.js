74f3b5e37ed669f5f4ccfef00c06e4cb88fa1bcc
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-20 20:17:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+function moveDodgerRight() {
+  var RightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(RightNumbers, 10)
  
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
   }
 }
 
