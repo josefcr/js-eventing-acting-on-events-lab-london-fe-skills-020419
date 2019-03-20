468ca8421bbfcbe8bf558f8ce1f7d143df5fd1ed
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-20 19:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,8 +0,0 @@
-function moveDodgerRight() {
-  var RightNumbers = dodger.style.left.replace('px', '')
-  var Right = parseInt(RightNumbers, 10)
- 
-  if (right > 0) {
-    dodger.style.right = `${right  1}px`
-  }
-}
